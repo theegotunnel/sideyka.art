4ca3bea5d4fdc7d5d99c11305be99d989e675517
Dmitriy Shedko <theegotunnel@gmail.com>
2023-06-23 17:05:53 +0300

    fix js links add video Fri Jun 23 17:05:53 BST 2023

--- js/script.js
+++ js/script.js
@@ -5,34 +5,6 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
-// Smooth scrolling animation
-
-const allLinks = document.querySelectorAll('a:link');
-
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
-
-    // Scroll back to top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
-    }
-
-    // Close mobile naviagtion
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  });
-});
-
 // Sticky navigation
 
 const sectionHeroEl = document.querySelector('.section-hero');
@@ -58,3 +30,32 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHeroEl);
+
+// need to fix
+
+// // Smooth scrolling animation
+// const allLinks = document.querySelectorAll('a:link');
+
+// allLinks.forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
+
+//     // Scroll back to top
+//     if (href === '#')
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//       });
+
+//     // Scroll to other links
+//     if (href !== '#' && href.startsWith('#')) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: 'smooth' });
+//     }
+
+//     // Close mobile naviagtion
+//     if (link.classList.contains('main-nav-link'))
+//       headerEl.classList.toggle('nav-open');
+//   });
+// });
